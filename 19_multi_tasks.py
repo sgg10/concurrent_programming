a894e7b8d45aab1ed83f52f92a96a7f5ee5c7214
Sebastian Granda Gallego <sebastian.granda@jikkosoft.com>
2021-07-22 08:58:16 -0500

    Multi tasks

---
+++ 19_multi_tasks.py
@@ -0,0 +1,37 @@
+from typing import Iterable, Union
+from logging_utils import info, THREAD_FORMAT
+import logging
+import threading
+from time import sleep
+from random import randint
+import requests
+from concurrent.futures import ThreadPoolExecutor, Future
+
+logging.basicConfig(level=logging.DEBUG, format=THREAD_FORMAT)
+
+URLS = [
+    'https://codigofacilito.com/',
+    'https://twitter.com/home/',
+    'https://www.google.com/',
+    'https://es.stackoverflow.com/',
+    'https://stackoverflow.com/',
+    'https://about.gitlab.com/',
+    'https://github.com/',
+    'https://www.youtube.com/',
+]
+
+def check_status_callback(result: Iterable[Union[requests.Response, str]]) -> None:
+    info(f"Server {result[1]} response is: {result[0].status_code}")
+
+if __name__ == "__main__":
+    with ThreadPoolExecutor(max_workers=2) as executor:
+        futures = [ executor.submit(lambda url: (requests.get(url), url), url) for  url in URLS ]
+        for future in futures:
+            future.add_done_callback(
+                lambda future: check_status_callback(future.result())
+            )
+
+        future = executor.submit(lambda a, b: a+b, randint(1,50), randint(1,50))
+        future.add_done_callback(
+            lambda future: info(f"Result: {future.result()}")
+        )
