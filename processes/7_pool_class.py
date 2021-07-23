6926bf0e20120099fea722558265203dd2ef70bd
Sebastian Granda Gallego <sebastian.granda@jikkosoft.com>
2021-07-23 13:07:14 -0500

    Async Objects

--- processes/7_pool_class.py
+++ processes/7_pool_class.py
@@ -11,6 +11,10 @@ def is_even(number):
     return number % 2 == 0, number
 
 if __name__ == "__main__":
+    # with Pool(processes=2) as executor:
+    #     result, number = executor.apply(is_even, args=(10,))
+    #     info(f'Is {number} even? {result}')
     with Pool(processes=2) as executor:
-        result, number = executor.apply(is_even, args=(10,))
-        info(f'Is {number} even? {result}')
+        apply_result = executor.apply_async(is_even, args=(10,))
+        apply_result.wait(timeout=5)
+        info(f'Is {apply_result.get(timeout=2)[1]} even? {apply_result.get(timeout=2)[0]}')
